15d3b0e478557cdfeaf7c83f87e40828f2ee301b
gaoxiang <815095642@qq.com>
2019-04-20 19:25:45 +0800

    取消数据库

--- filter/getFilter/controller/login/index.js
+++ filter/getFilter/controller/login/index.js
@@ -1,11 +1,11 @@
-const db = require('../../../../sql');
+// const db = require('../../../../sql');
 module.exports = async function (user) {
   //   res.send(req.path);
   console.log('login>', user.ip);
   // 添加向user表里添加一条记录
-  db.user.create({
-    user: 'gaox',
-    password: 'xxx'
-  });
+  // db.user.create({
+  //   user: 'gaox',
+  //   password: 'xxx'
+  // });
   return user;
 };
